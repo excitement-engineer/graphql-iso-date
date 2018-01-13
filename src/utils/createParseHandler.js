a62f7d5f6bd04cf5e3f66046651fe2a59ba37122
marcusstenbeck <marcus.stenbeck@gmail.com>
2018-01-14 00:14:05 +0100

    improve error message in parse handler

--- src/utils/createParseHandler.js
+++ src/utils/createParseHandler.js
@@ -10,14 +10,14 @@ export default function createParseHandler(
   return (value: mixed, ast: ?ASTNode): Date => {
     if (!(typeof value === 'string' || value instanceof String)) {
       throw new GraphQLError(
-        `${typeName} cannot represent non string type ${JSON.stringify(value)}`,
+        `Can't parse non-string type: ${JSON.stringify(value)}`,
         ast ? [ast] : undefined
       )
     }
 
     if (!validateValue(value)) {
       throw new GraphQLError(
-        `${typeName} cannot represent an invalid time-string ${JSON.stringify(value)}`,
+        `Can't parse an invalid ${typeName} string: ${JSON.stringify(value)}`,
         ast ? [ast] : undefined
       )
     }
